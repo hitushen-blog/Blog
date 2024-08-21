1caf2d871ee10de37ccb37f1ed0580f593671379
shenyunpeng <2287674693@qq.com>
2024-08-21 22:12:34 +0800

    修改部分信息

--- blog-view/src/views/Login.vue
+++ blog-view/src/views/Login.vue
@@ -30,8 +30,8 @@
 		data() {
 			return {
 				loginForm: {
-					username: 'Naccl',
-					password: '11'
+					username: 'admin',
+					password: '123456'
 				},
 				loginFormRules: {
 					username: [
